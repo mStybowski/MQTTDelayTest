b511db819e57367f8f75c3f406e9ea7f0d15fcc7
mbool <mstybowski@gmail.com>
2021-01-20 17:46:00 +0100

    added csv

--- server.js
+++ server.js
@@ -1,18 +1,48 @@
 const mqtt = require('mqtt')
 const client  = mqtt.connect('ws://localhost:8083/mqtt')
 
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+
 client.on('connect', function () {
-    client.subscribe('fromBrowser', function (err) {
+    client.subscribe(["fromBrowser", "results"], function (err) {
         if(!err)
             console.log("Server has connected to the broker!")
     })
 })
 
+function generateCSV(message){
+    console.log("YEA!!")
+    let myArrayOfStrings = message.split(',');
+    console.log(myArrayOfStrings);
+
+    let arrayOfValues = myArrayOfStrings.map(function(x) {
+        return {value:parseInt(x)}
+    });
+
+    console.log(arrayOfValues);
+
+    const csvWriter = createCsvWriter({
+        path: 'out.csv',
+        header: [
+            {id: 'value', title: 'Value'}
+        ]
+    });
+
+
+    csvWriter
+        .writeRecords(arrayOfValues)
+        .then(()=> console.log('The CSV file was written successfully'));
+}
+
 client.on('message', function (topic, message) {
 
+    if(topic !== "fromBrowser"){
+        generateCSV(message.toString())
+    }
     if(topic === "fromBrowser"){
         client.publish("fromServer", message.toString());
         console.log('Otrzymano wiadomosc: ' +  message.toString())
     }
+
 })
 
